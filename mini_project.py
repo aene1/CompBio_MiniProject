0341f857340749a6dabb87576ec70d4e4fcb49b8
aene1 <aene@luc.edu>
2021-03-01 11:36:57 -0600

    fixed extract_CDS and kallisto

--- mini_project.py
+++ mini_project.py
@@ -8,42 +8,50 @@ log_file = open('miniProject.log','w')
 
 
 #download the SRR and fastq them
-def download_data(SRR):
-    wget = 'wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/' +  SRR + '/' + SRR + '.1'
-    fastq = 'fastq-dump -I --split-files ' +SRR + '.1'
-    rename = 'mv ' + SRR + '.1' +' ' + SRR
-    os.system(wget)
-    os.system(rename)
-    os.system(fastq)
+# def download_data(SRR):
+#     wget = 'wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/' +  SRR + '/' + SRR + '.1'
+#     fastq = 'fastq-dump -I --split-files ' +SRR + '.1'
+#     rename = 'mv ' + SRR + '.1' +' ' + SRR
+#     os.system(wget)
+#     os.system(rename)
+#     os.system(fastq)
 
 
 #extract the CDS in order to construct the transcriptome
 def extract_CDS():
     Entrez.email = 'aene@luc.edu'
-    handle = Entrez.efetch(db = 'nucleotide', id = 'EF999921', rettype='fasta')
-    records = list(SeqIO.parse(handle,'fasta'))
+    handle = Entrez.efetch(db = 'nucleotide', id = 'EF999921', rettype='gb',retmode = 'text')
+    records = SeqIO.parse(handle,'genbank')
     count = 0
     outfile = open('CDS_EF999921.fa','w') #the outfile where the CDS records will  be written out to
-    for i in records.features:
-        if i.type == 'CDS':
-            outfile.write('>' + str(i.qualifiers['protein_id']) + '\n' + str(i.location.extract(i).seq)+'\n')
-            count += 1
+    for i in records:
+        for j in i.features:
+            if j.type == 'CDS':
+                outfile.write('>' + str(j.qualifiers['protein_id']).replace("'",'').replace('[','').replace(']','') + '\n' + str(j.location.extract(i).seq)+'\n')
+                count += 1
     log_file.write('The HCMV genome (EF999921) has ' + str(count) + '\n')
 
 
+#runn kallisto based on SRR numbers and the SRR files dowloaded before
+def kallisto(SRR):
+    kallisto_index = 'time kallisto index -i HCMVindex.idx CDS_EF999921.fa'
+    os.system(kallisto_index)
+    run_kallisto = 'time kallisto quant -i HCMVindex.idx -o ./' + SRR +' -b 30 -t 4 '+ SRR + '_1.fastq '+ SRR+ '_2.fastq'
+    os.system(run_kallisto)
 
 
 
 
 
 
-parser = argparse.ArgumentParser(description='Process some SRRs and split paired reads.')
-parser.add_argument('SRR', metavar='N', type=str, nargs='+',
-                    help='Compare SRR files')
-# parser.add_argument('--download_SRR')
-args = parser.parse_args()
 
-for i in args.SRR:
-    download_data(i)
+# parser = argparse.ArgumentParser(description='Process some SRRs and split paired reads.')
+# parser.add_argument('SRR', metavar='N', type=str, nargs='+',
+#                     help='Compare SRR files')
+# # parser.add_argument('--download_SRR')
+# args = parser.parse_args()
+
+# for i in args.SRR:
+#     download_data(i)
 
 extract_CDS()
