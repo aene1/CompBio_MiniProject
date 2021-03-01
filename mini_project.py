4353d2f1eb6a078049de70375abcf8b2e85bc81e
aene1 <aene@luc.edu>
2021-02-28 18:55:03 -0600

    added a parser

--- mini_project.py
+++ mini_project.py
@@ -1,23 +1,25 @@
+import argparse
 import os
 from Bio import SeqIO
 from Bio import Entrez
 
 log_file = open('miniProject.log','w')
 
-def arg_parser():
-    #sets up the argument parser and returns it so it can run forward"
-    parser = argparse.ArgumentParser(description = "Input sra files")
-    parser.add_argument(parser.add_argument('download_SRR_files', metavar = 'N/A', type=str, nargs = '*', help = 'SRR download')
-    return parser.parse_args()
 
-if args.donload_SRR_files != "N/A":
-    for i in args.do:
 
-    #finish this and change the parser
-
-def dowload_data(SRR):
-	 os.system('wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/' +  SRR + '/' + SRR + '.1')
-         os.system('fastq-dump -I --split-files' + SRR + '.1')
+def download_data(SRR):
+    wget = 'wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/' +  SRR + '/' + SRR + '.1'
+    fastq = 'fastq-dump -I --split-files' +SRR + '.1'
+    os.system(wget)
+    os.system(fastq)
 
 
 
+parser = argparse.ArgumentParser(description='Process some SRRs and split paired reads.')
+parser.add_argument('SRR', metavar='N', type=str, nargs='+',
+                    help='Compare SRR files')
+parser.add_argument('--download_SRR', dest='accumulate', action='store_const')
+
+args = parser.parse_args()
+
+download_data(args)
