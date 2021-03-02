e30a0aa89c3d046e6355a457a5534bc88eda12ef
aene1 <aene@luc.edu>
2021-03-02 17:43:10 -0600

    fix R

--- Sleuth.R
+++ Sleuth.R
@@ -1,5 +1,7 @@
 library(sleuth)
 library(data.table)
+#extract results and see most significants results
+library(dplyr)
 
 #read in the table you made describing samples and kallisto output 
 stab <- read.table("input_sleuth.txt", header=TRUE, stringsAsFactors = FALSE, sep='\t')
@@ -16,14 +18,13 @@ so <- sleuth_fit(so, ~1, 'reduced')
 #perform the likelihood ratio test for differential expression between conditions 
 so <- sleuth_lrt(so, 'reduced','full')
 
-#extract results and see most significants results 
-library(dplyr)
+
 #extract the test results from the sleuth object 
 sleuth_table <- sleuth_results(so, 'reduced:full','lrt',show_all=FALSE)
 #filter most significant results (FDR/qval < 0.05) and sort by pval
 sleuth_significant <- dplyr::filter(sleuth_table, qval <= 0.05) %>% dplyr::arrange(pval)
 
-sig_sleuth <- sleuth_significant %>% select(target_id, test_stat, pval, qval))
+sig_sleuth <- sleuth_significant %>% select(target_id, test_stat, pval, qval)
 #write target id, test stat, pval and qval for significant transcript
 #include header, tab-delimit
 write.table(sig_sleuth, file="R_sleuth.txt",quote= FALSE,row.names= FALSE)
