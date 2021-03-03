c225bbc471cab6a1bf40f3059b6f833931231051
aene1 <aene@luc.edu>
2021-03-03 03:22:41 -0600

    fixed bowtie

--- mini_project.py
+++ mini_project.py
@@ -83,9 +83,9 @@ def index_bowtie():
 
 
 def bowtie2(SRR):
-    build_bowtie2 = 'bowtie2-build EF999921.fa EF99992_1'
+    build_bowtie2 = 'bowtie2-build EF999921.fa EF999921_1'
     os.system(build_bowtie2)
-    bowtie2 = 'bowtie2 --quiet --no-unal --al-conc BOW_'+SRR+'.fastq -x EF99992_1 -1 '+SRR+ '_1.fastq -2'+SRR+'_2.fastq -S '+SRR+ '.sam'
+    bowtie2 = 'bowtie2 --quiet --no-unal --al-conc EF999921_' + SRR + '.fastq -x EF999921_1 -1 '+ SRR+ '.1_1.fastq -2 ' + SRR+ '.1_2.fastq -S EF999921_'
     os.system(bowtie2)
 
 
