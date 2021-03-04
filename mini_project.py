2fa928f668584a230f9dcb46e9a9639f463f6656
aene1 <aene@luc.edu>
2021-03-04 16:11:46 -0600

    added README.md

--- mini_project.py
+++ mini_project.py
@@ -162,7 +162,7 @@ def blast_longestcontigs():
     blastn_cmd = 'blastn -query ' + path + '/longest_contig.fasta -db '+ path + 'betaherpesvirinae -max_target_seqs 10 -out ' + path + '/blast_results.txt -outfmt "6 sacc pident length qstart qend sstart send bitscore evalue stitle"'
     os.system(blastn_cmd)
     log_file.write('sacc' + '\t' + 'pident' + '\t' + 'length' + '\t' + 'qstart' + '\t' + 'qend' + '\t' + 'sstart' + '\t' + 'send' + '\t' + 'bitscore' + '\t' + 'eval' + '\t' + 'stitle' + '\n')
-    read_blast_results = open('blast_results.txt').readlines()
+    read_blast_results = open(path + 'blast_results.txt', 'r').readlines()
     for i in read_blast_results:
         log_file.write(str(i))
         log_file.write('\n')
